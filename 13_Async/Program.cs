0d32f76c74104edc624e37942bd9b422001fe787
Jack McNeil <mcneil.jack.m@gmail.com>
2021-08-31 10:10:15 -0400

    13 asynch

---
+++ 13_Async/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to make a meal");
+            Console.ReadKey();
+
+            Kitchen kitchen = new Kitchen();
+            Potato potato = new Potato();
+
+            //Synchronous, can't do other tasks
+            potato.Peel();
+
+            //Asynchronous, so we can do other things
+            // Async needs to do tasks
+            // Tasks are "jobs" and not actual objects, yet.
+            var fries = kitchen.FryPotatoesAsync(potato);
+
+            //Synchronous, but can't start until our fries Task<> is finished because we need the result.
+            Hamburger hamburger = kitchen.AssembleHamburger();
+
+            if (!fries.IsCompleted)
+            {
+                Console.WriteLine("Waiting on fries");
+            }
+
+            kitchen.ServeMeal(fries.Result, hamburger);
+            Console.ReadKey();
+
+        }
+    }
+}
