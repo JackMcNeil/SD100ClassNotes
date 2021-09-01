84a83e46a27f73d49fc4c8d7b4781c804e0b308a
Jack McNeil <mcneil.jack.m@gmail.com>
2021-09-01 12:10:28 -0400

    14 Database RestaurantRater

---
+++ 14_RestaurantRater/Models/RestaurantDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace _14_RestaurantRater.Models
+{
+    public class RestaurantDBContext : DbContext
+    {
+        public RestaurantDBContext() : base("DefaultConnection") { }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+        public DbSet<Rating> Rating { get; set; }
+    }
+}
